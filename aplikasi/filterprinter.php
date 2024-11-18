292b7f8b48a855b9ee8e600c5a2bcbcf08913f53
ardana96 <ardanagama@gmail.com>
2024-11-18 13:50:00 +0700

    Enhancement Printer dan Scanner

--- aplikasi/filterprinter.php
+++ aplikasi/filterprinter.php
@@ -4,13 +4,24 @@ $nomor=$_GET['nomor'];
 $query="SELECT * from  printer WHERE nomor='".$nomor."' ";
 $get_data=mysql_query($query);
 $hasil=mysql_fetch_array($get_data);
+// $nomor=$hasil['nomor'];
+// $id_perangkat=$hasil['id_perangkat'];
+// $printer=$hasil['printer'];
+// $keterangan=$hasil['keterangan'];
+// $status=$hasil['status'];
+//$data=$nomor."&&&".$id_perangkat."&&&".$printer."&&&".$keterangan."&&&".$status;
 $nomor=$hasil['nomor'];
 $id_perangkat=$hasil['id_perangkat'];
 $printer=$hasil['printer'];
 $keterangan=$hasil['keterangan'];
 $status=$hasil['status'];
+$user = $hasil['user'];
+$lokasi = $hasil['lokasi'];
+$tgl_perawatan = $hasil['tgl_perawatan'];
+$bulan = $hasil['bulan'];
 
 
+$data=$nomor."&&&".$id_perangkat."&&&".$printer."&&&".$keterangan."&&&".$status."&&&".$user."&&&".$lokasi."&&&".$tgl_perawatan."&&&".$bulan;
 
-$data=$nomor."&&&".$id_perangkat."&&&".$printer."&&&".$keterangan."&&&".$status;
+
 echo $data; ?>
--- aplikasi/scanner.php
+++ aplikasi/scanner.php
@@ -20,12 +20,21 @@ http.send(null);}}
 function handleResponse(){
 if(http.readyState == 4){
 var string = http.responseText.split('&&&');
+// document.getElementById('nomor').value = string[0];  
+// document.getElementById('id_perangkat').value = string[1];
+// document.getElementById('printer').value = string[2]; 
+// document.getElementById('keterangan').value = string[3];
+// document.getElementById('status').value = string[4]; 
+
 document.getElementById('nomor').value = string[0];  
 document.getElementById('id_perangkat').value = string[1];
 document.getElementById('printer').value = string[2]; 
 document.getElementById('keterangan').value = string[3];
 document.getElementById('status').value = string[4]; 
-
+document.getElementById('user').value = string[5];
+document.getElementById('lokasi').value = string[6];
+document.getElementById('tgl_perawatan').value = string[7];
+document.getElementById('bulan').value = string[8];
 
 
 }}
@@ -164,7 +173,7 @@ function kdauto($tabel, $inisial){
                                             <input class="form-control" type="hidden" name="nomor" value="<? echo kdauto("scaner",""); ?>" readonly>
                                     
                                         </div>
-										 <div class="form-group">
+										 <!-- <div class="form-group">
                                          ID Perangkat
                                             <input class="form-control" type="text" name="id_perangkat" placeholder="ID Perangkat"  >
                                     
@@ -180,14 +189,70 @@ function kdauto($tabel, $inisial){
          Keterangan / Lokasi                                    
                                             <input  placeholder="Tempat Lokasi Printer" class="form-control" type="text" name="keterangan" >
                                     
+                                        </div>	 -->
+
+                                        <div class="form-group">
+                                         	ID Perangkat
+                                            <input class="form-control" type="text" name="id_perangkat" placeholder="ID Perangkat"  >
+                                    
+                                        </div>
+											
+										<div class="form-group">
+                                    		Scanner
+                                            <input class="form-control" type="text" name="printer" placeholder="ID Perangkat"  >
+                                    
+                                        </div>
+	
+										<div class="form-group">
+												Nama User                                    
+                                            <input  placeholder="Nama User" class="form-control" type="text" name="nama_user" >
+                                    
+                                        </div>
+										
+										<div class="form-group">
+												Lokasi                                    
+                                            <input  placeholder="Lokasi" class="form-control" type="text" name="lokasi" >
+                                    
+                                        </div>
+	
+										<div class="form-group">
+												 Keterangan                                    
+                                            <input  placeholder="Keterangan" class="form-control" type="text" name="keterangan" >
+                                    
+                                        </div>
+
+										 <div class="form-group">
+											Bulan Perawatan         
+											<select  class="form-control" name='bulan' >	 <option value=<? echo $id_bulan; ?>><? echo $namabulan; ?> </option>
+												
+												<?	$s2 = mysql_query("SELECT * FROM bulan  ");
+													if(mysql_num_rows($s2) > 0){
+													while($datas2 = mysql_fetch_array($s2)){
+													$id_bulan=$datas2['id_bulan'];
+													$bulan=$datas2['bulan'];
+													?>
+												 <option value="<? echo $id_bulan; ?>"> <? echo $bulan; ?>
+												 </option>
+												 
+												 <?}}?>
+												
+										
+											</select>        
+                                    
                                         </div>	
+                                        <div class="form-group">
+                                                    Tanggal Perawatan                                    
+                                            <input class="form-control" type="date" name="tgl_perawatan" >
+                                    
+                                        </div>
+                                        <div>
 										Status                                       
-        <select class="form-control" name='status' required="required">
-            <option value="EFRATA" >EFRATA</option>
-			<option value="RETAIL" >RETAIL</option>
-						
-    
-        </select>
+                                        <select class="form-control" name='status' required="required">
+                                            <option value="EFRATA" >EFRATA</option>
+                                            <option value="RETAIL" >RETAIL</option>
+                                                        
+                                    
+                                        </select>
 
 																			
          
@@ -221,7 +286,7 @@ function kdauto($tabel, $inisial){
                                             <input class="form-control" type="hidden" name="nomor" id="nomor" readonly>
                                     
                                         </div>
-										 <div class="form-group">
+										 <!-- <div class="form-group">
                                          ID Perangkat
                                             <input class="form-control" type="text" name="id_perangkat" id="id_perangkat"   >
                                     
@@ -237,16 +302,76 @@ function kdauto($tabel, $inisial){
          Keterangan / Lokasi                                    
                                             <input  placeholder="Tempat Lokasi Printer" class="form-control" type="text" name="keterangan" id="keterangan" >
                                     
-                                        </div>	
+                                        </div>	 -->
+										<div class="form-group">
+                                         ID Perangkat
+                                            <input class="form-control" type="text" name="id_perangkat" id="id_perangkat"   >
+                                    
+                                        </div>
+											
+										<div class="form-group">
+											Scanner
+                                            <input class="form-control" type="text" name="printer" id="printer" placeholder="ID Perangkat"  >
+                                    
+                                        </div>
+										<div class="form-group">
+											Nama User
+                                            <input class="form-control" type="text" name="user" id="user" placeholder="Nama User"  >
+                                        </div>
 										
+										<div class="form-group">
+											Lokasi
+                                            <input class="form-control" type="text" name="lokasi" id="lokasi" placeholder="Lokasi"  >
+                                        </div>
+	
+										<div class="form-group">
+											Keterangan                                  
+                                            <input  placeholder="Keteranan" class="form-control" type="text" name="keterangan" id="keterangan" >
+                                    
+                                        </div>	
 
-Status                                       
-        <select class="form-control" name='status' id='status' required="required">
-             <option value="GARMENT" >GARMENT</option>
-			 <option value="TEXTILE" >TEXTILE</option>
-			
-    
-        </select>
+										<div class="form-group">
+											Tanggal Perawatan
+                                            <input class="form-control" type="date" name="tgl_perawatan" id="tgl_perawatan" placeholder="hh-mm-yyyy"  >
+                                        </div>
+
+										<div class="form-group">
+											Bulan Perawatan                                       
+											<select class="form-control" name='bulan' id='bulan' required="required">
+												<option value="" ></option>
+												<option value="01" >JANUARI</option>
+												<option value="02" >FEBRUARI</option>
+												<option value="03" >MARET</option>
+												<option value="04" >APRIL</option>
+												<option value="05" >MEI</option>
+												<option value="06" >JUNI</option>
+												<option value="07" >JULI</option>
+												<option value="08" >AGUSTUS</option>
+												<option value="09" >SEPTEMBER</option>
+												<option value="10" >OKTOBER</option>
+												<option value="11" >NOVEMBER</option>
+												<option value="12" >DESEMBER</option>
+													
+												
+										
+											</select>
+										</div>
+                                        <div>
+                                        Status                                       
+                                            <!-- <select class="form-control" name='status' id='status' required="required">
+                                                <option value="GARMENT" >GARMENT</option>
+                                                <option value="TEXTILE" >TEXTILE</option>
+                                                
+                                        
+                                            </select> -->
+
+                                            Status                                       
+                                            <select class="form-control" name='status' id='status' required="required">
+                                                <option value="EFRATA" >EFRATA</option>
+                                                <option value="RETAIL" >RETAIL</option>
+                                                
+                                        
+                                            </select>
                                        
                                 
                                         </div>
